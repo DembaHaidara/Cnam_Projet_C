324f6d39efc4ef242d32072469e92e95349a80d9
Haidara Demba <91568514+DembaHaidara@users.noreply.github.com>
2023-02-10 06:44:45 +0100

    Add files via upload

--- ProjetC/main.cpp
+++ ProjetC/main.cpp
@@ -1,15 +1,15 @@
-#include <iostream> //  écrire file
-#include <fstream> //lire file
+#include <iostream>
+#include <fstream>
 #include <string>
-#include <math.h>  // fonction maths (racine carré sqrt)
-#include <wchar.h> // Caractère large
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
-//Fonction qui permet de convertir les caractère avec accent en caractère simple
-char conversionChar(char c) {
-    char caracter = c;
+// Fonction qui permet de convertir les caractères avec accent/majuscule en caractère simple
+char Conversion_Char(char c) {
 
+    char caracter = tolower(c);
     // a
     if (caracter == -126 || caracter == -128 || caracter == -124 || caracter == -94 || caracter == -96 || caracter == -92) {
         caracter = 97;
@@ -26,12 +26,12 @@ char conversionChar(char c) {
     }
 
     // o
-    else if (caracter == -76 || caracter == -108 ) {
+    else if (caracter == -76 || caracter == -108 || caracter == -77) {
         caracter = 111;
     }
 
     // u
-    else if (caracter == -101 || caracter == -103 || caracter == -69 || caracter == -71) {
+    else if (caracter == -101 || caracter == -103 || caracter == -69 || caracter == -71 || caracter == -70) {
         caracter = 117;
     }
 
@@ -40,247 +40,287 @@ char conversionChar(char c) {
         caracter = 99;
     }
 
+    //n
+    else if (caracter == -79) {
+        caracter = 110;
+    }
+
   return caracter;
 
 }
-//Fonction qui permet de compter le nb d'occurence de chaque lettre de l'alphabet d un ficher txt puis
-// l ecrit dans le ficher correspondant
+
+// Fonction qui permet de compter le nombre d'occurrences de chaque lettre de l'alphabet d'un fichier texte puis l'écrit dans le fichier correspondant
 int * Occurrences_Lettres(string urlFile) {
-  //Déclaration d'un flux permettant de lire dans le fichier
-  ifstream monFlux(urlFile); //pour lire le file
-  ofstream monFluxWrite(urlFile, ios::app); //pour write avec 2 eme parametre qui signifie a la fin du ficher
 
-  /*
-  3 facons de lire un file :
-
-  Ligne par ligne, en utilisant getline().
-  Mot par mot, en utilisant les chevrons >>  .
-  Caractère par caractère, en utilisant get()
-  */
-
+  ifstream monFlux(urlFile);
+  ofstream monFluxWrite(urlFile, ios::app);
   char letter, letter2;
-
-
-  int static tabOccurence[26] = {}; //tableau quii contiendra le nb d occurence
+  int static tabOccurrences[26] = {};
   char tabLetter[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-  string tableau = "["; // tableau quii contiendra le nb d occurence en mode string , va nous servie pour l enregistrer dans le file
+  string tabSave = "[";
 
-  if (monFlux && monFluxWrite) { // je verifie si l ouverture du ficher en ecriture et lecture est possible
-    while (monFlux.get(letter)) { // je recupere dans le ficher caractere par caractere
+  if (monFlux && monFluxWrite) {
+    while (monFlux.get(letter)) {
 
-      letter2 = conversionChar(letter); // je convertir tout les caracter spacial et me retourne la valeur sans accent
+      letter2 = Conversion_Char(letter);
       for (int i = 0; i < 26; i++) {
-        if (tabLetter[i] == letter2) { //des que je croisse la lettre je l enregistre dans le tab
-          tabOccurence[i] += 1;
+
+        if (tabLetter[i] == letter2) {
+
+          tabOccurrences[i] += 1;
         }
+
       }
+
     }
-    monFluxWrite << "Occurences du texte à analyser : ";
+
+    monFluxWrite << "Occurrences du texte à analyser : ";
+
     for (int i = 0; i < 26; i++) {
 
-      tableau += to_string(tabOccurence[i]); // me convertie mon tableau de int en string
+      tabSave += to_string(tabOccurrences[i]);
+      monFluxWrite << tabLetter[i] << ":" << tabOccurrences[i] << " ; ";
 
-      monFluxWrite << tabLetter[i] << ":" << tabOccurence[i] << " ; "; //permet d'ecricre les occurence dans le ficher txt  write << data
-
-      // Si j arrive a l indice 25 , ne me concatene pas la virgule
       if (i == 25) {} else {
-        tableau += ",";
+        tabSave += ",";
       }
-
     }
-    tableau += "]";
+
+    tabSave += "]";
+
+    cout << "" << endl;
+
     monFluxWrite << " " << endl;
-    monFluxWrite << "Tableau d'occurences : " << tableau << endl;// ecrir dans le file write << data
-    return tabOccurence; // je retounre le tableu d occurence(int) au main
-  } else {// si il ne peut pas lire et ecrir dans le file return 0
+
+    monFluxWrite << "Tableau d'occurrences : " << tabSave << endl;
+
+    cout << "" << endl;
+
+    return tabOccurrences;
+
+  } else {
+
     return 0;
+
   }
-  cout << "Une erreur c'est produite" << endl;
+
+  cout << "Une erreur s’est produite" << endl;
+
   cout << "Nous vous prions de nous excuser pour la gêne occasionnée" << endl;
+
   exit(0);
 
 }
 
-//Fonction qui contient les tableau par defaux des 5 text langue
-int * recupTabFile(string urlFile) {
+// Fonction qui contient les tableaux par défaut des 5 textes des fichiers langues
+
+int * ReturnTabFiles(string urlFile) {
 
   if (urlFile == "francais.txt") {
-    int static tab[26] = {83,15,38,78,235,8,21,8,113,6,0,135,46,95,70,48,4,79,182,66,124,32,0,8,2,0};
+
+    int static tab[26] = {396,47,122,165,800,50,37,35,295,27,0,249,132,330,238,122,30,304,353,280,270,67,0,19,15,0};
     return tab;
+
   } else if (urlFile == "allemand.txt") {
-    int static tab[26] = {123,50,49,74,259,49,47,77,150,0,26,54,24,223,37,13,0,98,91,89,88,9,11,2,5,37};
+
+    int static tab[26] = {311,78,141,239,926,53,126,237,455,4,49,141,135,560,121,30,1,440,344,344,201,38,52,0,1,50};
     return tab;
+
   } else if (urlFile == "italien.txt") {
-    int static tab[26] = {203,10,94,71,20513,26,17,163,4,3,108,43,123,170,54,8,128,87,102,62,26,4,5,9,17};
+
+    int static tab[26] = {570,44,216,153,544,39,108,41,540,0,1,279,172,346,489,122,11,273,209,287,130,81,1,0,1,29};
     return tab;
+
   } else if (urlFile == "espagnol.txt") {
-    int static tab[26] = {43,4,16,18,53,2,5,4,27,1,0,20,8,30,35,9,2,21,30,11,19,7,1,0,1,3};
+
+    int static tab[26] = {448,60,168,168,495,28,35,33,257,14,1,195,149,279,339,110,33,237,269,158,161,45,0,2,64,10};
     return tab;
+
   } else if (urlFile == "anglais.txt") {
-    int static tab[26] = {116,27,48,67,219,35,36,66,129,7,19,57,33,111,91,23,0,93,135,145,59,19,24,4,32,0};
+
+    int static tab[26] = {283,43,96,102,378,62,72,127,222,5,16,154,94,259,244,54,0,198,208,267,68,36,43,4,70,1};
     return tab;
+
   }
+
   else {
+
     return 0;
+
   }
+
 }
 
-// Permet de calculer l'ecart
-double Calcule_ecart(int * textTabAnalyse, string urlLangue) {
+// Fonction qui permet de calculer l'écart
+double Calcule_Ecart(int * texteTabAnalyse, string urlLangue) {
 
-  int * tabLangue = recupTabFile(urlLangue + ".txt"); //recup la langue du tableau par défaut qui a été généré
-
-  double sommeTextAnalyser = 0;
-
+  int * tabLangue = ReturnTabFiles(urlLangue + ".txt");
+  double sommeTexteAnalyser = 0;
   double sommeTabLangue = 0;
-
-  int nbElementTabText = 26;
-
+  int nbElementTabTexte = 26;
   double resultatTabLangue = 0;
   double calculTabLangue = 0;
 
-  for (int i = 0; i < nbElementTabText; i++) {
-    sommeTextAnalyser += textTabAnalyse[i]; // additionner toutes les occurences a=3 , b=6 , c=0 ça nous fera 3+6+0 du texte à analyser
-
-    sommeTabLangue += tabLangue[i]; // additionner toutes les occurences a=3 , b=6 , c=0 ça nous fera 3+6+0 de la langue
-
+  for (int i = 0; i < nbElementTabTexte; i++) {
+    sommeTexteAnalyser += texteTabAnalyse[i];
+    sommeTabLangue += tabLangue[i];
   }
 
-  for (int i = 0; i < nbElementTabText; i++) {
-
-    calculTabLangue += pow((textTabAnalyse[i] / sommeTextAnalyser) - (tabLangue[i] / sommeTabLangue), 2.0); // 2.0 = au carré
-
+  for (int i = 0; i < nbElementTabTexte; i++) {
+    calculTabLangue += pow((texteTabAnalyse[i] / sommeTexteAnalyser) - (tabLangue[i] / sommeTabLangue), 2.0);
   }
 
-  resultatTabLangue = sqrt(calculTabLangue); // racine carré
+  resultatTabLangue = sqrt(calculTabLangue);
 
-  cout << "Ecart " << urlLangue << " " << resultatTabLangue << endl;
-
+  cout << "Ecart " << urlLangue << " " << fixed << setprecision(2) << resultatTabLangue << endl;
   return resultatTabLangue;
-
 };
 
-void Trouve_langue(int * p, string urlFile) {
+
+
+// Fonction qui permet de retourner la langue du texte
+void Trouve_Langue(int * adresseTab, string urlFile) {
 
   string urlLangue[5] = {"allemand","espagnol","francais","italien","anglais"};
-  double resultatLangue[5]; //définit la taille du tab vide, et contiendra le stockage de tous les calculs ecart de chaque langue
+  double resultatLangue[5];
 
-  // p = Occurrences_Lettres(urlFile);
   for (int i = 0; i < 5; i++) {
-    resultatLangue[i] = Calcule_ecart(p, urlLangue[i]); //return le resultat de calcul ecart par rapport a la langue de l'indice du tab
-
+    resultatLangue[i] = Calcule_Ecart(adresseTab, urlLangue[i]);
   };
 
-  int positionLangue; //indice du tableau resultat langue
-
-  double mini = resultatLangue[0]; // valeur plus petite est à l'indice 0
-
+  int positionLangue;
+  double resultatPetit = resultatLangue[0];
   int i;
- // ce for ci dessous me permet de recupere la plus petit valeur
+
   for (i = 0; i < 5; i++) {
-    if (resultatLangue[i] < mini) {// si la valeur de l'indice actuelle est plus petit que l indice par defaux alors rentre dans le if
-      mini = resultatLangue[i]; //save la valeur la plus petit
-      positionLangue = i; // retenir l'indice du plus petit
-
+    if (resultatLangue[i] < resultatPetit) {
+      resultatPetit = resultatLangue[i];
+      positionLangue = i;
     }
-
   }
 
   cout << "Langue trouvée : " << urlLangue[positionLangue] << endl;
+  ofstream monFluxWrite(urlFile, ios::app);
 
-  ofstream monFluxWrite(urlFile, ios::app);// (ecrit dans le fil , a la fin du ficher)
   if (monFluxWrite) {
 
     monFluxWrite << "Langue trouvée : " << urlLangue[positionLangue] << endl;
-    monFluxWrite << urlLangue[0] << "; écart : " << resultatLangue[0] << endl;
-    monFluxWrite << urlLangue[1] << "; écart : " << resultatLangue[1] << endl;
-    monFluxWrite << urlLangue[2] << "; écart : " << resultatLangue[2] << endl;
-    monFluxWrite << urlLangue[3] << "; écart : " << resultatLangue[3] << endl;
-    monFluxWrite << urlLangue[4] << "; écart : " << resultatLangue[4] << endl;
+
+    monFluxWrite << urlLangue[0] << "; écart : " <<  fixed << setprecision(2) << resultatLangue[0] << endl;
+
+    monFluxWrite << urlLangue[1] << "; écart : " <<  fixed << setprecision(2) << resultatLangue[1] << endl;
+
+    monFluxWrite << urlLangue[2] << "; écart : " <<  fixed << setprecision(2) << resultatLangue[2] << endl;
+
+    monFluxWrite << urlLangue[3] << "; écart : " <<  fixed << setprecision(2) << resultatLangue[3] << endl;
+
+   monFluxWrite << urlLangue[4] << "; écart : " <<  fixed << setprecision(2) << resultatLangue[4] << endl;
 
   } else {
-    cout << "Une erreur c'est produite" << endl;
+
+    cout << "Une erreur s’est produite" << endl;
     cout << "Nous vous prions de nous excuser pour la gêne occasionnée" << endl;
     exit(0);
+
   }
 
 }
 
+
 int main() {
-  int choix; // choix selon les options 1,2,3
+
+  int choixUtilisateur;
   string nomFile;
   string cheminAcces;
   string urlFile;
 
-  do { //tant que c'est pas bon il va re-executer, recommence cette partie
-    cout << "Que souhaitez -vous générer ?" << endl;
+  do {
+
+    cout << "Que souhaitez-vous générer ?" << endl;
     cout << "• Les occurrences des lettres d'un texte : Option (1)" << endl;
     cout << "• Trouver la langue d'un texte: Option (2)" << endl;
     cout << "• Quitter : Option (3)" << endl;
 
-    cin >> choix;
-    cout << "• Choix selectionné : " << choix << endl;
+    cin >> choixUtilisateur;
+    cout << "• Choix selectionner : " << choixUtilisateur << endl;
+    cout <<""<<endl;
+
   }
 
-  while ((choix != 1) && (choix != 2) && (choix != 3) && isdigit(choix) != 0); // tant que c pas 1,2,3 on va re executer le do
+  while ((choixUtilisateur != 1) && (choixUtilisateur != 2) && (choixUtilisateur != 3) && isdigit(choixUtilisateur) != 0);
 
-   int *p;
-  if (choix == 1) {
-
-
-    do {
-      cout << "• Veuillez me renseigne le chemin d'accès du ficher !" << endl;
-      cin >> cheminAcces;
-      cout << "• Veuillez me renseigne le nom du ficher à généré et son extension !" << endl;
-      cin >> nomFile;
-    }
-
-    while (cheminAcces.empty() == true && urlFile.empty() == true); // empty = vide
-    urlFile = cheminAcces+nomFile;
-
-    p = Occurrences_Lettres(urlFile);
-    if (p == 0) { // dans la fonction occurence_lettre, si arrive pas a ouvrir ou a écrire le fichier : return 0
-      cout << "Impossible d'ouvrir le ficher !!" << endl;
-      cout << "Veuillez vérifier le chemin ou accès du ficher saisie !!!" << endl;
-            cout << "Vous avez saisie : "<< urlFile<< endl;
-
-    } else {
-      cout << "• Ficher généré avec succès !" << endl;
-    }
-
-  } else if (choix == 2) {
-
-
+  int *adresseTab;
+  if (choixUtilisateur == 1) {
 
     do {
-      cout << "• Veuillez me renseigne le chemin d'accès du ficher !" << endl;
+
+      cout << "• Veuillez me renseigner le chemin d'accès du fichier !" << endl;
       cin >> cheminAcces;
-      cout << "• Veuillez me renseigner le nom du ficher à analyser et son extension !" << endl;
+      cout <<""<<endl;
+      cout << "• Veuillez me renseigner le nom du fichier à générer et son extension !" << endl;
       cin >> nomFile;
+
     }
 
     while (cheminAcces.empty() == true && urlFile.empty() == true);
 
     urlFile = cheminAcces+nomFile;
+    adresseTab = Occurrences_Lettres(urlFile);
 
-      p = Occurrences_Lettres(urlFile);
+    if (adresseTab == 0) {
 
-
-    if (p == 0) {
       cout << "Impossible d'ouvrir le ficher !!" << endl;
-      cout << "Veuillez vérifier le chemin ou accès du ficher saisie !!!" << endl;
+      cout << "Veuillez vérifier le chemin ou accès du fichier saisie !!!" << endl;
       cout << "Vous avez saisie : "<< urlFile<< endl;
 
     } else {
 
-      Trouve_langue(p, urlFile);
+      cout << "• Ficher généré avec succès !" << endl;
+
     }
-  } else if (choix == 3) {
-    cout << "Aurevoir :)" << endl;
+
+  } else if (choixUtilisateur == 2) {
+
+    do {
+      cout << "• Veuillez me renseigner le chemin d'accès du ficher !" << endl;
+      cin >> cheminAcces;
+      cout <<""<<endl;
+      cout << "• Veuillez me renseigner le nom du fichier à analyser et son extension !" << endl;
+      cin >> nomFile;
+
+    }
+
+    while (cheminAcces.empty() == true && urlFile.empty() == true);
+
+    urlFile = cheminAcces+nomFile;
+    adresseTab = Occurrences_Lettres(urlFile);
+
+
+    if (adresseTab == 0) {
+
+      cout << "Impossible d'ouvrir le fichier !!" << endl;
+      cout << "Veuillez vérifier le chemin ou accès du fichier saisie !!!" << endl;
+      cout << "Vous avez saisie : "<< urlFile<< endl;
+
+    } else {
+
+      Trouve_Langue(adresseTab, urlFile);
+
+    }
+
+  } else if (choixUtilisateur == 3) {
+    cout << "Au revoir" << endl;
+
   } else {
-    cout << "Une erreur c'est produite" << endl;
+
+    cout << "Une erreur s’est produite" << endl;
+
     cout << "Nous vous prions de nous excuser pour la gêne occasionnée" << endl;
+
     exit(0);
+
   }
+
 }
+
